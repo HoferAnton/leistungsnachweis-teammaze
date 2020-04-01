7317a0cf168a7475ac568ab4bec990fa4f0ab2bd
electron2302 <technikon23@gmail.com>
2020-04-01 14:57:52 +0200

    Added second solver (dumb but parallel)

--- main.go
+++ main.go
@@ -1,9 +1,4 @@
 package main
 
-import "fmt"
-
 func main() {
-	a := int64(-1)
-	fmt.Printf("%v", uint(a))
-	fmt.Printf("%v", uint(a))
 }
--- solver/solver.go
+++ solver/solver.go
@@ -2,6 +2,8 @@ package solver
 
 import (
 	"fmt"
+	"sync"
+
 	"github.com/ob-algdatii-20ss/leistungsnachweis-teammaze/common"
 )
 
@@ -15,18 +17,19 @@ func DFS(lab common.Labyrinth, from common.Location, to common.Location) []commo
 	return rdfs(lab, from, to, []common.Location{})
 }
 
-func rdfs(lab common.Labyrinth, from common.Location, to common.Location, dontTouch []common.Location) []common.Location {
-
+func rdfs(lab common.Labyrinth, from common.Location, to common.Location,
+	dontTouch []common.Location) []common.Location {
 	fmt.Printf("From %v | To: %v \n", from, to)
 
 	if from.Compare(to) {
-		return append([]common.Location{to})
+		return []common.Location{to}
 	}
 
 	for _, neighbor := range lab.GetConnected(from) {
 		if neighbor.Compare(from) || contains(dontTouch, neighbor) {
 			continue
 		}
+
 		dontTouch = append(dontTouch, neighbor)
 		if result := rdfs(lab, neighbor, to, dontTouch); result != nil {
 			return append(result, from)
@@ -36,11 +39,59 @@ func rdfs(lab common.Labyrinth, from common.Location, to common.Location, dontTo
 	return nil
 }
 
+// solver 2
+func PDS(lab common.Labyrinth, from common.Location, to common.Location) []common.Location {
+	var (
+		result []common.Location
+		wg     sync.WaitGroup
+	)
+
+	wg.Add(1)
+	pd(lab, from, to, []common.Location{}, &wg, &result)
+
+	wg.Wait()
+
+	return result
+}
+
+func pd(lab common.Labyrinth, from common.Location, to common.Location,
+	way []common.Location, wg *sync.WaitGroup, result *[]common.Location) {
+	if *result != nil {
+		wg.Add(-1)
+		return
+	}
+
+	fmt.Printf("From %v | To: %v | Way: %v \n", from, to, way)
+
+	way = append(way, from)
+
+	if from.Compare(to) {
+		*result = way
+
+		wg.Add(-1)
+
+		return
+	}
+
+	for _, neighbor := range lab.GetConnected(from) {
+		if contains(way, neighbor) {
+			continue
+		}
+
+		wg.Add(1)
+
+		go pd(lab, neighbor, to, way, wg, result)
+	}
+
+	wg.Add(-1)
+}
+
 func contains(l []common.Location, e common.Location) bool {
 	for _, s := range l {
 		if s.Compare(e) {
 			return true
 		}
 	}
+
 	return false
 }
