2f4f616cbc52d1d647fc7717104e449c69fa4d83
Benjamin Hartwig <b.hartwig@hm.edu>
2020-04-02 11:12:59 +0200

    Added example with two cubes fixed rendering moved rendering logic (of cubes) to cube.go moved from rendering with gl.TRIANGLE_STRIP to gl.TRIANGLES

--- display/shaders/simple_vertex.glsl
+++ display/shaders/simple_vertex.glsl
@@ -1,11 +1,8 @@
 #version 330
-//uniform mat4 projection;
-//uniform mat4 camera;
-//uniform mat4 model;
+uniform mat4 mvp;
 
 in vec3 position;
 
 void main() {
-//    gl_Position = projection * camera * model * vec4(vert, 1);
-    gl_Position = vec4(position, 1);
+    gl_Position = mvp * vec4(position, 1);
 }
