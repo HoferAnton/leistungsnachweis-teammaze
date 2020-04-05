4e661ed74d9da5b363f6e73e3ada46b17b741d9f
electron2302 <technikon23@gmail.com>
2020-04-06 00:07:58 +0200

    Fixing unnecessary merge issue

--- main.go
+++ main.go
@@ -1,4 +1,23 @@
 package main
 
 func main() {
+		/*
+		res, _ := printer.Print2D(
+			generator.NewDepthFirstGenerator().GenerateLabyrinth(
+				common.NewLocation(
+					uint(30),
+					uint(20),
+					uint(0))))
+		fmt.Printf("%v", res)
+	*/
+	/*
+		maxLoc := common.NewLocation(15,7,26)
+		x := uint(12)
+		y := uint(5)
+		z := uint(23)
+		loc := common.NewLocation(x,y,z)
+		index := common.GetIndex(x, y, z, maxLoc)
+		reLoc := common.GetLocation(index, maxLoc)
+		fmt.Printf("The loc: %v has the index: %v and from the index we got: %v", loc, index, reLoc)
+	*/
 }
