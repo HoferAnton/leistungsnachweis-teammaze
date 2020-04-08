6afd56a268e61e3509f8847c18aa89d94fd870c5
Benjamin Hartwig <b.hartwig@hm.edu>
2020-04-08 18:43:29 +0200

    cleaned up main.go

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"github.com/ob-algdatii-20ss/leistungsnachweis-teammaze/common"
 	"log"
 	"math/rand"
 	"os"
 	"runtime"
 	"time"
+
+	"github.com/ob-algdatii-20ss/leistungsnachweis-teammaze/common"
 
 	"github.com/gotk3/gotk3/glib"
 	"github.com/gotk3/gotk3/gtk"
@@ -18,11 +19,21 @@ const appID = "com.github.ob-algdatii-20ss.leistungsnachweis-teammaze"
 const labyrinthSize = 5
 const numConnections = 100
 
+type directionEnum int
+
+const (
+	right    directionEnum = 0
+	left     directionEnum = 1
+	up       directionEnum = 2
+	down     directionEnum = 3
+	backward directionEnum = 4
+	forward  directionEnum = 5
+)
+
 func main() {
 	runtime.LockOSThread()
 
-	log.Println(os.Args[0])
-
+	log.Println("Execution path: ", os.Args[0])
 	log.SetOutput(os.Stdout)
 
 	application, err := gtk.ApplicationNew(appID, glib.APPLICATION_FLAGS_NONE)
@@ -43,22 +54,7 @@ func main() {
 
 			randLoc := common.NewLocation(randX, randY, randZ)
 
-			var randLoc2 common.Location
-
-			switch rand.Intn(6) {
-			case 0:
-				randLoc2 = common.NewLocation(randX+1, randY, randZ)
-			case 1:
-				randLoc2 = common.NewLocation(randX-1, randY, randZ)
-			case 2:
-				randLoc2 = common.NewLocation(randX, randY+1, randZ)
-			case 3:
-				randLoc2 = common.NewLocation(randX, randY-1, randZ)
-			case 4:
-				randLoc2 = common.NewLocation(randX, randY, randZ+1)
-			case 5:
-				randLoc2 = common.NewLocation(randX, randY, randZ-1)
-			}
+			randLoc2 := getRandomNeighboringLocation(randX, randY, randZ)
 
 			lab.Connect(randLoc, randLoc2)
 		}
@@ -80,4 +76,23 @@ func main() {
 	display.FatalIfError("Shutdown Signal Connection failed: ", err)
 
 	os.Exit(application.Run(os.Args))
+}
+
+func getRandomNeighboringLocation(randX uint, randY uint, randZ uint) common.Location {
+	switch directionEnum(rand.Intn(6)) { //nolint:gomnd Number of directions in three dimensions is always 6
+	case right:
+		return common.NewLocation(randX+1, randY, randZ)
+	case left:
+		return common.NewLocation(randX-1, randY, randZ)
+	case up:
+		return common.NewLocation(randX, randY+1, randZ)
+	case down:
+		return common.NewLocation(randX, randY-1, randZ)
+	case backward:
+		return common.NewLocation(randX, randY, randZ+1)
+	case forward:
+		return common.NewLocation(randX, randY, randZ-1)
+	default:
+		panic("Rolled an unknown direction")
+	}
 }
