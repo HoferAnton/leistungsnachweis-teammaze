a11beb28e4530788a212d2be8fb7e70b2c43498f
Benjamin Hartwig <b.hartwig@hm.edu>
2020-04-08 10:38:16 +0200

    Rotating Labyrinth with correct diffuse & specular lighting, no shadows.

--- display/cube.go
+++ display/cube.go
@@ -9,12 +9,33 @@ import (
 	"github.com/go-gl/mathgl/mgl32"
 )
 
+var vertices = []mgl32.Vec3{
+	{0.5, 0.5, 0.5},
+	{0.5, 0.5, -0.5},
+	{0.5, -0.5, 0.5},
+	{0.5, -0.5, -0.5},
+	{-0.5, 0.5, 0.5},
+	{-0.5, 0.5, -0.5},
+	{-0.5, -0.5, 0.5},
+	{-0.5, -0.5, -0.5},
+}
+
+var drawingIndices = []uint8{
+	5, 3, 1,
+	5, 7, 3,
+	5, 7, 4,
+	7, 4, 6,
+	6, 4, 0,
+	6, 0, 2,
+	0, 3, 2,
+	0, 1, 3,
+	4, 5, 0,
+	5, 1, 0,
+	6, 7, 2,
+	7, 3, 2,
+}
+
 type Cube struct {
-	Center   mgl32.Vec3
-	Vertices []mgl32.Vec3
-
-	drawingIndices []uint8
-
 	vao                    uint32
 	vbo                    uint32
 	vboIndices             uint32
@@ -22,6 +43,8 @@ type Cube struct {
 	viewMatUniformID       int32
 	modelMatUniformID      int32
 	lightPositionUniformID int32
+
+	Transform Transform
 
 	shaderProgram uint32
 }
@@ -31,45 +54,17 @@ func NewCube(x, y, z, xSize, ySize, zSize float32, shaderProgram uint32) Cube {
 		panic("Negative Size given")
 	}
 
-	xSize /= 2
-	ySize /= 2
-	zSize /= 2
-
-	center := mgl32.Vec3{
-		x, y, z,
-	}
-
 	cube := Cube{
-		Center: center,
-		Vertices: []mgl32.Vec3{
-			{x + xSize, y + ySize, z + zSize},
-			{x + xSize, y + ySize, z - zSize},
-			{x + xSize, y - ySize, z + zSize},
-			{x + xSize, y - ySize, z - zSize},
-			{x - xSize, y + ySize, z + zSize},
-			{x - xSize, y + ySize, z - zSize},
-			{x - xSize, y - ySize, z + zSize},
-			{x - xSize, y - ySize, z - zSize},
-		},
-		drawingIndices: []uint8{
-			5, 3, 1,
-			5, 7, 3,
-			5, 7, 4,
-			7, 4, 6,
-			6, 4, 0,
-			6, 0, 2,
-			0, 3, 2,
-			0, 1, 3,
-			4, 5, 0,
-			5, 1, 0,
-			6, 7, 2,
-			7, 3, 2,
-		},
 		shaderProgram:          shaderProgram,
 		mvpUniformID:           gl.GetUniformLocation(shaderProgram, gl.Str("MVP\x00")),
 		viewMatUniformID:       gl.GetUniformLocation(shaderProgram, gl.Str("V\x00")),
 		modelMatUniformID:      gl.GetUniformLocation(shaderProgram, gl.Str("M\x00")),
 		lightPositionUniformID: gl.GetUniformLocation(shaderProgram, gl.Str("lightPosition_worldSpace\x00")),
+		Transform: Transform{
+			translation: mgl32.Translate3D(x, y, z),
+			scale:       mgl32.Scale3D(xSize, ySize, zSize),
+			rotation:    mgl32.QuatIdent(),
+		},
 	}
 
 	generateAndInitializeBuffers(&cube)
@@ -95,9 +90,9 @@ func generateAndInitializeBuffers(cube *Cube) {
 	gl.GenBuffers(1, &cube.vboIndices)
 	gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, cube.vboIndices)
 
-	numVertexBufferBytes := len(cube.Vertices) * int(unsafe.Sizeof(mgl32.Vec3{}))
-	gl.BufferData(gl.ARRAY_BUFFER, numVertexBufferBytes, gl.Ptr(cube.Vertices), gl.STATIC_READ)
-	gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, len(cube.drawingIndices), gl.Ptr(cube.drawingIndices), gl.STATIC_READ)
+	numVertexBufferBytes := len(vertices) * int(unsafe.Sizeof(mgl32.Vec3{}))
+	gl.BufferData(gl.ARRAY_BUFFER, numVertexBufferBytes, gl.Ptr(vertices), gl.STATIC_READ)
+	gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, len(drawingIndices), gl.Ptr(drawingIndices), gl.STATIC_READ)
 }
 
 func (cube Cube) draw(view mgl32.Mat4, projection mgl32.Mat4, labCenter mgl32.Vec3, lightPosition mgl32.Vec3, timeSinceStart time.Duration) {
@@ -107,7 +102,7 @@ func (cube Cube) draw(view mgl32.Mat4, projection mgl32.Mat4, labCenter mgl32.Ve
 	gl.UseProgram(cube.shaderProgram)
 
 	model := mgl32.HomogRotate3DY(float32(timeSinceStart.Seconds())).
-		Mul4(mgl32.Translate3D(-labCenter.X(), -labCenter.Y(), -labCenter.Z()))
+		Mul4(mgl32.Translate3D(-labCenter.X(), -labCenter.Y(), -labCenter.Z()).Mul4(cube.Transform.AsMatrix()))
 	mvp := projection.Mul4(view.Mul4(model))
 
 	gl.UniformMatrix4fv(cube.mvpUniformID, 1, false, &mvp[0])
@@ -116,7 +111,7 @@ func (cube Cube) draw(view mgl32.Mat4, projection mgl32.Mat4, labCenter mgl32.Ve
 
 	gl.Uniform3fv(cube.lightPositionUniformID, 1, &lightPosition[0])
 
-	gl.DrawElements(gl.TRIANGLES, int32(len(cube.drawingIndices)), gl.UNSIGNED_BYTE, gl.PtrOffset(0))
+	gl.DrawElements(gl.TRIANGLES, int32(len(drawingIndices)), gl.UNSIGNED_BYTE, gl.PtrOffset(0))
 
 	gl.BindVertexArray(0)
 }
@@ -130,5 +125,5 @@ func (cube Cube) String() string {
 		vaoValidness = "valid"
 	}
 
-	return fmt.Sprintf("Cube at (%v) with %s vao.", cube.Center, vaoValidness)
+	return fmt.Sprintf("Cube at (%v) with %s vao.", cube.Transform.translation.Col(3), vaoValidness)
 }
