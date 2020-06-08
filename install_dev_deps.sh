7e9a0b5ed18d633f46902ab8188333c88eaf32ec
AndHager <andreas.polling@web.de>
2020-06-08 16:42:13 +0200

    removed sudo

--- install_dev_deps.sh
+++ install_dev_deps.sh
@@ -1,1 +1,1 @@
-sudo apt-get update && sudo apt-get install -y libgl1-mesa-dev libgtk-3-dev libcairo2-dev libglib2.0-dev
+apt-get update && apt-get install -y libgl1-mesa-dev libgtk-3-dev libcairo2-dev libglib2.0-dev
